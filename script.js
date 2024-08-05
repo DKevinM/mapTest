04640b074591478431f9cba0554b39af269942ac
DKevinM <35807537+DKevinM@users.noreply.github.com>
2024-08-05 08:17:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const options = {
-    key: 'PsLAtXpsPTZexBwUkO7Mx5I',
+    key: 'yVY0cXn4PmZ0G3BjUvQ7IpSqOG48bgiL',
     // Changing Windy parameters at start-up time
     // (recommended for faster start-up)
     lat: 50.4,
