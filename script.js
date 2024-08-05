d48fdee3353c4d0866c8f121136adfaa3053df44
DKevinM <35807537+DKevinM@users.noreply.github.com>
2024-08-05 09:15:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const options = {
     key: 'yVY0cXn4PmZ0G3BjUvQ7IpSqOG48bgiL',
-    // Changing Windy parameters at start-up time
-    // (recommended for faster start-up)
     lat: 53.535645,
     lon: -113.47194,
     zoom: 5,
-    hourFormat: '12h',
 };
 
 windyInit(options, windyAPI => {
@@ -15,18 +12,4 @@ windyInit(options, windyAPI => {
         .setContent('Windy map integrated!')
         .openOn(map);
 
-    const { store } = windyAPI;
-    
-    const levels = store.getAllowed('availLevels');
-
-    let i = 0;
-    setInterval(() => {
-        i = i === levels.length - 1 ? 0 : i + 1;
-
-        store.set('level', levels[i]);
-    }, 5000);
-
-    store.on('level', level => {
-        console.log(`Level was changed: ${level}`);
-    });
 });
